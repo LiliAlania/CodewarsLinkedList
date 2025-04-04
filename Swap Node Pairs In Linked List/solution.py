70efca8df864a7c49859ca19dee5db7bf6f6fb41
LiliAlania <alaniia.pn@ucu.edu.ua>
2025-04-04 19:20:00 +0300

    Create solution.py

---
+++ Swap Node Pairs In Linked List/solution.py
@@ -0,0 +1,15 @@
+def swap_pairs(head):
+    new_head = head.next  
+    prev = None
+    current = head
+    while current and current.next:
+        next_pair = current.next.next
+        second = current.next
+        second.next = current
+        current.next = next_pair
+        if prev:
+            prev.next = second
+        prev = current
+        current = next_pair
+    return new_head 
+    return head
