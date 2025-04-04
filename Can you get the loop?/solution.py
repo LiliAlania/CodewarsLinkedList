1e05331af02709900efdc41b0b865ea53368fc82
LiliAlania <alaniia.pn@ucu.edu.ua>
2025-04-04 19:52:18 +0300

    Update solution.py
    first cycle with slow and fast

--- Can you get the loop?/solution.py
+++ Can you get the loop?/solution.py
@@ -1,2 +1,12 @@
 def loop_size(node):
     slow, fast = node, node
+    while fast and fast.next:
+        slow = slow.next
+        fast = fast.next.next
+        if slow == fast:
+            break
+    else:
+        return 0
+    count = 1
+    fast = fast.next
+    return count
